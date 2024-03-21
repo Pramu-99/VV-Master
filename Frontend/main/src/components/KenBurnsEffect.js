387f3d81a05b8ebbc3226b33d72050f31931b78d
Tenura Pasandul <57010235+TenuraPasandul@users.noreply.github.com>
2024-03-21 14:46:38 +0530

    signup,home
    submitted those pages

---
+++ Frontend/main/src/components/KenBurnsEffect.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './KenBurnsEffect.css'; 
+import logo from '../img/logo.png'
+const KenBurnsEffect = () => {
+  return (
+    <div id="CrossFade">
+      <img src="//unsplash.it/1600/900?image=1043" alt="img" />
+      <img src="//unsplash.it/1600/900?image=1039" alt="img" />
+      <img src="//unsplash.it/1600/900?image=1017" alt="img" />
+      <img src="//unsplash.it/1600/900?image=929" alt="img" />
+      <div className="intro">
+        
+        
+        <p>
+        We are the official Camera Club of the Faculty of Applied Science, University of Vavuniya
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default KenBurnsEffect;
