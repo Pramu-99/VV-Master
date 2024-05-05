1b84847f018274100e2ff40bdb694f796c262419
Tenura Pasandul <57010235+TenuraPasandul@users.noreply.github.com>
2024-05-05 17:37:44 +0530

    posts.js
    this is post.js of models in backend

---
+++ Backend/models/posts.js
@@ -0,0 +1,49 @@
+const mongoose=require('mongoose');
+
+const postSchema=new mongoose.Schema({
+    regno:{
+        type:String,
+        required:true
+    },
+    fname:{
+        type:String,
+        required:true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    
+    pass:{
+        type:String,
+        required:true
+    },
+    
+    linkedin:{
+        type:String,
+        required:true
+    },
+    facebook:{
+        type:String,
+        required:true
+    },
+    instagram:{
+        type:String,
+        required:true
+    },
+    approval:{
+        type:Boolean,
+        required:true
+    }
+    
+});
+
+module.exports=mongoose.model('posts',postSchema);
