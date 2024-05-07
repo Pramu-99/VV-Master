007f24246d07b152966947f37aa344b0a460854c
TJeevana <jee200007@gmail.com>
2024-05-07 10:03:41 +0530

    updated
    function for users created

--- Frontend/main/src/components/UserProfile.js
+++ Frontend/main/src/components/UserProfile.js
@@ -1,2 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
+const UserProfile = (props) => {
+  
+    const memreg=props.memreg;
+    console.log(props.memreg);
+    
+    const [review,setReview]=useState([]);
+    const [ratedavg,setRatedavg]=useState(0);
+    
+    
+    const renderStars = (rating) => {
+      const stars = [];
+      for (let i = 1; i <= 5; i++) {
+        stars.push(
+          <span key={i} style={{ color: i <= rating ? 'gold' : 'gray' }}>
+            &#9733;
+          </span>
+        );
+      }
+      return stars;
+    };
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+        
+          
+          const response = await axios.get('http://localhost:8000/review');
+          
+          console.log('API Response:', response.data);
+          setReview(response.data);
+          console.log(review);
+        } catch (error) {
+          console.error('Error fetching members:', error.message);
+        }
+      };
+  
+      fetchData();
+      
+    }, []);
