2ec2aa1e637a73bae5601c7832601c3854b9a28b
TJeevana <jee200007@gmail.com>
2024-05-07 10:10:57 +0530

    memberprogress
    started creating

---
+++ Frontend/main/src/components/MemberProgress.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import UserProfile from './UserProfile'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import MemberEventDetails from './MemberEventDetails';
+import UserNavbar from './UserNavbar';
