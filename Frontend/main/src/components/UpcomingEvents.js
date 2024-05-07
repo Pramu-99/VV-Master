e7c42ceea4450b989a33925bab45e8870cd38e49
Jasmini Srinivasakan <87084622+Jasmini99@users.noreply.github.com>
2024-05-07 10:22:52 +0530

    Update UpcomingEvents.js
    created function for upcoming events

--- Frontend/main/src/components/UpcomingEvents.js
+++ Frontend/main/src/components/UpcomingEvents.js
@@ -9,3 +9,34 @@ import Event from "../img/apply.jpg";
 import UserNavbar from "./UserNavbar";
 
 import axios from 'axios';
+
+
+function UpcomingEvents() {
+
+
+
+  const memRegNo=localStorage.getItem('username');
+
+  localStorage.setItem('username', memRegNo);
+
+  console.log(memRegNo);
+
+  const Swal = require('sweetalert2');
+
+  const [eventDetails, setEventDetails] = useState(["nimal"]);
+
+  const [formData, setFormData] = useState({
+
+    userregno: memRegNo,
+
+    event: '',
+
+    roleapply: '',
+
+    status: false,
+
+    marks:0,
+
+    
+
+  });
