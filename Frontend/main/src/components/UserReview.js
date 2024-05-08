1eefc5c7959918a035469aa27440f3ae5f45cea9
Roshni23Angelin <87080905+Roshni23Angelin@users.noreply.github.com>
2024-05-08 13:44:08 +0530

    added few styles
    few css code

--- Frontend/main/src/components/UserReview.js
+++ Frontend/main/src/components/UserReview.js
@@ -2,3 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { FaStar } from "react-icons/fa";
 import UserNavbar from "./UserNavbar";
 import axios from 'axios';
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a2a2a2",
+  lightGreen: "#afdd37",
+  lightPink: "#f3f1ff",
+};
