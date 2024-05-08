80c3878521ab6d62ff80a2c0d5294b93642e6250
Roshni23Angelin <87080905+Roshni23Angelin@users.noreply.github.com>
2024-05-08 13:46:55 +0530

    Update UserReview.js
    minor updates added

--- Frontend/main/src/components/UserReview.js
+++ Frontend/main/src/components/UserReview.js
@@ -32,3 +32,66 @@ function UserReview() {
   const [members, setMembers] = useState(["nimal"]); // Corrected the initialization here
   
   const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/posts');
+        // Assuming response.data is an array or has an array property
+        console.log('API Response:', response.data);
+        setMembers(response.data);
+        
+      } catch (error) {
+        console.error('Error fetching members:', error.message);
+      }
+    };
+
+    fetchData();
+    console.log(members.status);
+  }, []);
+  
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData.regno);
+    console.log(formData.rate);
+    console.log(currentValue);
+    console.log(formData.reviewmsg);
+  };
+
+  const savePostToMongo = async () => {
+    try {
+      
+      await axios.post('http://localhost:8000/review/save', formData);
+      Swal.fire({
+        title: 'Review Successfully',
+        text: 'Thank you for Reviewing',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+      setFormData({
+        userregno:memRegNo,
+        regno:'',
+        role:'',
+        rate:'',
+        reviewmsg:'',
+      });
+      //navigate('/login');
+      /*console.log('Post saved successfully');*/
+    } catch (error) {
+      Swal.fire({
+        title: 'Error',
+        text: 'Please check your internet Connection',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+      console.error('Error saving post:', error.message);
+    }
+  };
+
+  
+
+  const onSubmit = () => {
+    //console.log(formData);
+    console.log(regNo);
+    console.log(currentValue);
+    savePostToMongo();
+  };
