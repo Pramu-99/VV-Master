4bc89476f3c951c842526822504c1de721c5b6ff
sajahanmasma <87080094+sajahanmasma@users.noreply.github.com>
2024-05-10 19:12:34 +0530

    Update UserLogin.js
    user login function created

--- Frontend/main/src/components/UserLogin.js
+++ Frontend/main/src/components/UserLogin.js
@@ -7,3 +7,13 @@ import '../css/font-awesome-4.7.0/css/font-awesome.min.css';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import UserNavbar from './UserNavbar';
+
+
+const UserLogin = () => {
+  const [regno, setRegno] = useState("");
+    const [pass, setPass] = useState("");
+    
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
