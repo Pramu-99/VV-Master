ca767c8e04046127bb315e8e62ae3eedc2d05f2d
Sithruby <87075876+Sithruby@users.noreply.github.com>
2024-05-11 11:43:27 +0530

    Updated Camera_reserve.js
    started creating function

--- Frontend/main/src/components/Camera_reserve.js
+++ Frontend/main/src/components/Camera_reserve.js
@@ -6,3 +6,20 @@ import vanni from "../img/unsplash_oQl0eVYd_n8.png";
 import axios from 'axios';
 import CommonNav from './CommonNav';
 import UserNavbar from './UserNavbar';
+export default function Camera_reserve(){
+
+  const Swal = require('sweetalert2');
+
+  const [equipment, setEquipment] = useState(["nimal"]);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const memRegNo=localStorage.getItem('username');
+  localStorage.setItem('username', memRegNo);
+  console.log(memRegNo);
+
+  const [formData, setFormData] = useState({
+    itemid:'',
+    userregno:memRegNo,
+    date:'',
+    purpose:'',
+  });
