173b9a49b8d03d385ad3e5be10682daac34542f0
Sithruby <87075876+Sithruby@users.noreply.github.com>
2024-05-11 11:49:37 +0530

    Finished CameraItem.js
    Finished Creating Camera Item Page

--- Frontend/main/src/components/CameraItem.js
+++ Frontend/main/src/components/CameraItem.js
@@ -4,3 +4,14 @@ const checkValidity=(event)=>{
         onCheckboxChange(event);
     }
 }
+return(
+<div className="container_re">
+    <div style={{backgroundImage:`url(${image})`, borderRadius:'5px 5px 0 0', backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",width:"100%",height:"200px"
+   }}  ></div>
+    <div style={{margin:' 10px',fontWeight:'bold'}}> {brand} {camname} {item}<br/> {des}</div>
+   <div style={{  border:'1px solid  #D2D0D0', borderRadius: "5px",margin:'0 10px',padding:'5px'}}><input type="checkbox" value={itemid}  onClick={onCheckboxChange}/>&larr;Click here</div>
+</div>
+);
+} 
