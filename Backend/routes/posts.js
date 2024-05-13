a565b008418169e3fa09499e68c409c70609f2dd
Tenura Pasandul <57010235+TenuraPasandul@users.noreply.github.com>
2024-05-13 15:34:49 +0530

    update post.js
    get method of post.js

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -3,7 +3,7 @@ const Posts = require('../models/posts');
 
 const router = express.Router();
 
-
+// save post
 router.post('/post/save', async (req, res) => {
     try {
         const newPost = new Posts(req.body);
@@ -18,11 +18,27 @@ router.post('/post/save', async (req, res) => {
     }
 });
 
+//get posts
+
+router.get('/posts', async (req, res) => {
+    try {
+        const posts = await Posts.find().exec();
+        return res.status(200).json({
+            success: true,
+            existingPosts: posts
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+});
 
 
-  
-  
+
+
+
+ 
 
 module.exports = router;
-
 
