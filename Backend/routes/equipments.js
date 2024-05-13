13eea97b9bf26a3be5b86ba50f2f2e3d86bdde1b
Pramu-99 <pramuxjaya99@gmail.com>
2024-05-13 15:53:11 +0530

    CRUD Started
    post event started

--- Backend/routes/equipments.js
+++ Backend/routes/equipments.js
@@ -2,3 +2,17 @@ const express = require('express');
 const Posts = require('../models/equipments');
 const router = express.Router();
 
+router.post('/equipment/save', async (req, res) => {
+    try {
+        const newPost = new Posts(req.body);
+        await newPost.save();
+        return res.status(200).json({
+            success: "Post saved successfully"
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+});
+
