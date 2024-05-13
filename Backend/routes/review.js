ff224a0602367d3739189fcb5f1f4930de6977b2
sajahanmasma <87080094+sajahanmasma@users.noreply.github.com>
2024-05-13 17:10:35 +0530

    Update review.js
    delete route finished

--- Backend/routes/review.js
+++ Backend/routes/review.js
@@ -31,3 +31,25 @@ router.get('/review', async (req, res) => {
         });
     }
 });
+
+router.delete('/review/delete/:id', async (req, res) => {
+    try {
+        const deletedPost = await Posts.findByIdAndDelete(req.params.id).exec();
+        
+        if (!deletedPost) {
+            return res.status(404).json({
+                message: "Post not found"
+            });
+        }
+
+        return res.json({
+            message: "Delete successful",
+            deletedPost
+        });
+    } catch (err) {
+        return res.status(400).json({
+            message: "Delete unsuccessful",
+            error: err.message
+        });
+    }
+});
