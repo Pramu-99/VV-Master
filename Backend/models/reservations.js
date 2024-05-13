4a03ba1fcac41d5adb8a34fe57eb1bb56d45a2c0
Roshni23Angelin <87080905+Roshni23Angelin@users.noreply.github.com>
2024-05-13 17:59:05 +0530

    Update reservations.js
    done

--- Backend/models/reservations.js
+++ Backend/models/reservations.js
@@ -1,1 +1,23 @@
 const mongoose=require('mongoose');
+const reservationSchema=new mongoose.Schema({
+    itemid:{
+        type:Array,
+        required:true
+    },
+    userregno:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    purpose:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:Boolean,
+        required:true
+    }
+});
