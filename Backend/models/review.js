09a7b22a55a402c8aa35befbc4404d4b895260af
Roshni23Angelin <87080905+Roshni23Angelin@users.noreply.github.com>
2024-05-13 18:00:29 +0530

    Update review.js

--- Backend/models/review.js
+++ Backend/models/review.js
@@ -1,1 +1,23 @@
 const mongoose=require('mongoose');
+const reviewSchema=new mongoose.Schema({
+    userregno:{
+        type:String,
+        required:true
+    },
+    regno:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true
+    },
+    rate:{
+        type:Number,
+        required:true
+    },
+    reviewmsg:{
+        type:String,
+        required:true
+    }
+});
