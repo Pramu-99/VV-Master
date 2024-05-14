0cd2a0acae8cf5fec31061175004189a54934c36
Sithruby <87075876+Sithruby@users.noreply.github.com>
2024-05-14 19:29:51 +0530

    Update reservations.js
    Post Request Done

--- Backend/routes/reservations.js
+++ Backend/routes/reservations.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const Posts = require('../models/reservations');
 const router = express.Router();
+
+router.post('/reservation/save', async (req, res) => {
+    try {
+        const newPost = new Posts(req.body);
+        await newPost.save();
+        return res.status(200).json({
+            success: "Post saved successfully"
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+});
