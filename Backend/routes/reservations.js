ced9d9175f0f6366d44c2933f4a32185f397e7b7
Sithruby <87075876+Sithruby@users.noreply.github.com>
2024-05-14 19:30:13 +0530

    Update reservations.js
    get request done

--- Backend/routes/reservations.js
+++ Backend/routes/reservations.js
@@ -15,3 +15,17 @@ router.post('/reservation/save', async (req, res) => {
         });
     }
 });
+
+router.get('/reservations', async (req, res) => {
+    try {
+        const posts = await Posts.find().exec();
+        return res.status(200).json({
+            success: true,
+            existingPosts: posts
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+});
