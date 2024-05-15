89a1749c3b775b4e750bad6ffa479d772432121a
Tenura Pasandul <57010235+TenuraPasandul@users.noreply.github.com>
2024-05-15 23:56:29 +0530

    create Admin notification page
    admin

---
+++ Frontend/main/src/components/admin/AdminNotification.js
@@ -0,0 +1,249 @@
+import React, { useState, useEffect } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import './styles.css'
+import AdminNavbar from './AdminNavbar';
+
+export default function AdminNotification() {
+    const [members, setMembers] = useState([]);
+    const [allMem,setAllMem] = useState([]);
+    const [applyMem,setApplyMem] = useState([]);
+    const [reservationMem,setReservationMem] = useState([]);
+    const [equipments,setEquipments] = useState([]);
+    const [formData, setFormData] = useState({
+      regno: '',
+      pass: '',
+      linkedin: '',
+      facebook: '',
+      instagram: '',
+      fname: '',
+      lname: '',
+      phone: '',
+      email: '',
+      approval: '',
+    });
+
+    const [applyFormData,setApplyFormData] = useState({
+      userregno:'',
+      event:'',
+      roleapply:'',
+      status:'',
+      marks:'',
+
+    });
+
+    const [reserveFormData,setReserveFormData] = useState({
+      itemid:'',
+      userregno:'',
+      date:'',
+      purpose:'',
+      status:'',
+    })
+    useEffect(() => {
+        fetchData();
+      }, []);
+
+    const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:8000/posts');
+          const applyMembers = await fetch('http://localhost:8000/applies');
+          const reservation = await fetch('http://localhost:8000/reservations');
+          const equipment = await fetch('http://localhost:8000/equipments');
+          if (response.ok) {
+            const data = await response.json();
+            const applyData = await applyMembers.json();
+            const reservationData = await reservation.json();
+            const equipmentData = await equipment.json();
+
+            setMembers(data.existingPosts.filter(member => !member.approval));
+            setAllMem(data.existingPosts);
+            setApplyMem(applyData.existingPosts.filter(apply => !apply.status));
+            setReservationMem(reservationData.existingPosts.filter(reserve=> !reserve.status));
+            setEquipments(equipmentData.existingPosts);
+
+          } else {
+            console.error('Failed to fetch members:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching members:', error.message);
+        }
+    };
+
+    const handleApprove = async (event) => {
+        try {
+          const updatedFormData = { ...formData, ...event, approval: true };
+          const response = await fetch(`http://localhost:8000/post/update/${event._id}`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(updatedFormData),
+          });
+          if (response.ok) {
+            console.log('Data updated successfully!');
+            // Refetch data to update UI
+            fetchData();
+          } else {
+            console.error('Failed to update data:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error updating data:', error.message);
+        }
+    }
+
+    const handleApproveReserve = async (event) => {
+      try {
+        const updatedFormData = { ...reserveFormData, ...event, status: true };
+        const response = await fetch(`http://localhost:8000/reservation/update/${event._id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedFormData),
+        });
+        if (response.ok) {
+          console.log('Data updated successfully!');
+          // Refetch data to update UI
+          fetchData();
+        } else {
+          console.error('Failed to update data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error updating data:', error.message);
+      }
+  }
+
+  const handleApproveApply = async (event) => {
+    try {
+      const updatedFormData = { ...applyFormData, ...event, status: true };
+      const response = await fetch(`http://localhost:8000/apply/update/${event._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedFormData),
+      });
+      if (response.ok) {
+        console.log('Data updated successfully!');
+        // Refetch data to update UI
+        fetchData();
+      } else {
+        console.error('Failed to update data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error updating data:', error.message);
+    }
+}
+
+const handleDeleteMember = async (memId) => {
+  try {
+      const response = await fetch(`http://localhost:8000/post/delete/${memId}`, {
+          method: 'DELETE',
+      });
+      if (response.ok) {
+          fetchData();
+      } else {
+          console.error('Failed to delete Reservation');
+      }
+  } catch (error) {
+      console.error('Error deleting reservation:', error);
+  }
+};
+
+const handleDeleteApply = async (applyId) => {
+  try {
+      const response = await fetch(`http://localhost:8000/apply/delete/${applyId}`, {
+          method: 'DELETE',
+      });
+      if (response.ok) {
+          fetchData();
+      } else {
+          console.error('Failed to delete Reservation');
+      }
+  } catch (error) {
+      console.error('Error deleting reservation:', error);
+  }
+};
+
+  const handleDeleteReserve = async (reserveId) => {
+    try {
+        const response = await fetch(`http://localhost:8000/reservation/delete/${reserveId}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            fetchData();
+        } else {
+            console.error('Failed to delete Reservation');
+        }
+    } catch (error) {
+        console.error('Error deleting reservation:', error);
+    }
+  };
+
+  const isLoggedIn = !!localStorage.getItem('username');
+  const isLoggedInRole = !!localStorage.getItem('role');
+  if (!isLoggedIn || !isLoggedInRole) {
+      
+      return (
+          <div style={styles.restrict}>
+              <h2><b><i class="bi bi-exclamation-octagon"></i> Restriction!</b></h2>
+              <h3>Please log in to access this page - <a href='/'>Click Here To Visit Login...</a> </h3>
+          </div>
+      );
+  }
+    return (
+      <div>
+        <AdminNavbar/><br/><br/>
+      <div className='container py-4 bg-body'>
+
+          <h2>Registration Approval</h2>
+          <div className="container py-4 bg-body">
+              
+              <div className="row">
+                  <Accordion defaultActiveKey={['0']} alwaysOpen>
+                  {Array.isArray(members) && members.length > 0 ? (
+                      members.map((event, index) => (
+                          <Accordion.Item key={index} eventKey={index.toString()}>
+                              <Accordion.Header style={{ fontSize: '20px' }}>{event.regno}</Accordion.Header>
+                              <Accordion.Body>
+                                  Name: {event.fname} {event.lname}<br />
+                                  Phone No: {event.phone}<br />
+                                  email: {event.email}<br />
+                                  Linkedin Link: {event.linkedin}<br />
+                                  Facebook Link: {event.facebook}<br />
+                                  Instagram Link: {event.instagram}<br />
+                                  Role: {event.role}<br/>
+                                  <button className='btn btn-primary btn-approve text-capitalize' onClick={() => handleApprove(event)}>Approve</button>
+                                  <button className='btn btn-danger text-capitalize btn-red' onClick={() => handleDeleteMember(event._id)}>Reject</button>
+                              </Accordion.Body>
+                          </Accordion.Item>
+                      ))
+                  ) : <p>No any non-approval members found.</p>
+                  }
+                  </Accordion>
+              </div>
+              
+          </div>
+          
+              </div>
+              
+          </div>
+          <footer className="text-center fixed-bottom mb-3" id='footer'>
+                  <p>&copy; Vanni Vogue Camera Club</p>
+          </footer>
+      </div>
+      </div>
+    )
+}
+
+
+const styles={
+  restrict:{
+  boxShadow: '2px 2px 10px 2px rgba(0, 0, 0, 0.582)',
+  padding: '20px 10px',
+  width: '90%',
+  marginLeft: '5%',
+  marginTop: '15%',
+  backgroundColor: '#fff',
+  height:'20vh',
+  }
+}
