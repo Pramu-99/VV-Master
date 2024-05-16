da0700539e64a45b733dab1bb1aff1b7a1cf1c4b
rathant98 <87080569+rathant98@users.noreply.github.com>
2024-05-16 07:25:56 +0530

    functions

--- Frontend/main/src/components/AboutUs.js
+++ Frontend/main/src/components/AboutUs.js
@@ -1,1 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image from './assets/features.jpg';
 
+function AboutUs() {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
+  useEffect(() => {
+    // Smooth scroll effect
+    const handleScroll = (event) => {
+      event.preventDefault();
+      const targetId = event.currentTarget.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    document.querySelectorAll('a.scroll-link').forEach(anchor => {
+      anchor.addEventListener('click', handleScroll);
+    });
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      document.querySelectorAll('a.scroll-link').forEach(anchor => {
+        anchor.removeEventListener('click', handleScroll);
+      });
+    };
+  }, []);
